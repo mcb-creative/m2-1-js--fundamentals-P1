7f0338c341e4d59ccec808cef64c8aad5fe87e1e
Mallory Belgica <mallorybelgica@gmail.com>
2020-09-19 15:13:28 -0400

    Completed exercise 6.4

--- __workshop/exercise-6.4.js
+++ __workshop/exercise-6.4.js
@@ -1,11 +1,15 @@
+//complete
 function exercise6_4() {
-  // Exercise 6.4
+  // Exercise 6.4 - complete
   //
   // Write a loop that will print to the console
   // all of the EVEN integers from 36 to 0.
   //
   // Write code between the lines (below)
   // -----------------------------------------------------------------
+  for (let num = 36; num >= 0; num -= 2) {
+    console.log(num);
+  }
   // -----------------------------------------------------------------
   // Write code between the lines (above)
   //
